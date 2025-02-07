ab47c5abfbabc9c6e73377193d60465533ccef20
danaportenier <danaportenier@icloud.com>
2025-02-07 14:10:48 -0500

    Initial Commit

---
+++ WeightHistoryGrapher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeightHistoryGrapher
+//
+//  Created by Dana Portenier on 2/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeightHistoryGrapher/WeightHistoryGrapherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightHistoryGrapherApp.swift
+//  WeightHistoryGrapher
+//
+//  Created by Dana Portenier on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct WeightHistoryGrapherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
